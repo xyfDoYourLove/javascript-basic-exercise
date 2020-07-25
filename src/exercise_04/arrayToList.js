105b1283d556ea2c6bc9cd4bfbc4849c97d9d8a8
DoYourLove <xyf15620168540@163.com>
2020-07-25 21:18:58 +0800

    [XiaoYiFan] feat: 完成exercise-04

--- src/exercise_04/arrayToList.js
+++ src/exercise_04/arrayToList.js
@@ -5,6 +5,30 @@ export default function arrayToList(array) {
   //
   // * Please implement this function and pass all the tests in array_to_list_spec.js.
   // * Please do NOT modify the signature of the function.
+  class List {
+    constructor(value, next) {
+      this.value = value;
+      this.next = next;
+    }
+  }
+  if (array === undefined || array === null) {
+    throw new Error('Creating list from undefined array');
+  }
+  if (array.length === 0) {
+    throw new Error('Creating list from empty array');
+  }
 
-  throw new Error('Please delete this line and implement the function');
+  let list;
+  let currElement;
+  for (let i = 0; i < array.length; i += 1) {
+    if (i === 0) {
+      list = new List(array[i], null);
+      currElement = list;
+    } else {
+      currElement.next = new List(array[i], null);
+      currElement = currElement.next;
+    }
+  }
+
+  return list;
 }
