f7b0e98b5e0ceae649e87056ac470938a7299ffd
DoYourLove <xyf15620168540@163.com>
2020-07-25 20:33:21 +0800

    [XiaoYiFan] feat:完成Exercise-03

--- src/exercise_03/rangeAndSum.js
+++ src/exercise_03/rangeAndSum.js
@@ -7,7 +7,24 @@ export function range(start, end) {
   // * Please implement this function and pass all the tests in range_and_array_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  if (start === end) {
+    return [];
+  }
+  if (start > end) {
+    const result = Array.of(start - end);
+    for (let k = end + 1; k <= start; k += 1) {
+      result[start - k] = k;
+    }
+    return result;
+  }
+
+  const result = Array.of(end - start);
+  if (end > start) {
+    for (let i = start; i < end; i += 1) {
+      result[i - start] = i;
+    }
+  }
+  return result;
 }
 
 export function sum(...numbers) {
@@ -17,6 +34,15 @@ export function sum(...numbers) {
   //
   // * Please implement this function and pass all the tests in range_and_array_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
+  let sumNum = 0;
+  if (numbers === undefined) {
+    return 0;
+  }
+  if (numbers.length === 1) {
+    return numbers[0];
+  }
+  for (let i = 0; i < numbers.length; i += 1) {
+    sumNum += numbers[i];
+  }
+  return sumNum;
 }
