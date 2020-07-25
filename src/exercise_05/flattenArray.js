9c6a060835484aca92b34b3858c5c45b8fbd14f8
DoYourLove <xyf15620168540@163.com>
2020-07-25 21:51:18 +0800

    [XiaoYiFan] feat：完成exercise-05

--- src/exercise_05/flattenArray.js
+++ src/exercise_05/flattenArray.js
@@ -6,5 +6,22 @@ export default function flattenArray(array) {
   // * Please implement this function and pass all the tests in flatten_array_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  if (array === undefined || array === null) {
+    throw new Error('Flatten undefined or null array');
+  }
+
+  const rultArray = [];
+  for (let i = 0; i < array.length; i += 1) {
+    if (array[i] instanceof Array) {
+      if (array[i].length !== 0) {
+        for (let k = 0; k < array[i].length; k += 1) {
+          rultArray[rultArray.length] = array[i][k];
+        }
+      }
+    } else {
+      rultArray[rultArray.length] = array[i];
+    }
+  }
+
+  return rultArray;
 }
